0267d52f0b8201fbd7ae05cfbd3042969f7bba14
pedro <pedrohperoni@gmail.com>
2022-01-31 21:21:14 -0300

    add global style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style/reset.css";
+import "./style/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
